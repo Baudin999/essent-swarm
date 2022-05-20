6a4cb2da06287f89834646692920d611ebeb8fb4
baudin999 <carlos.kelkboom@essent.nl>
2022-05-20 16:23:58 +0200

    trying to fix the azure functions

--- api/pageVisitTrigger/index.js
+++ api/pageVisitTrigger/index.js
@@ -2,17 +2,24 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-        if (name) {
-            context.bindings.outputDocument = JSON.stringify({
-                // create a random ID
-                id: new Date().toISOString() + Math.random().toString().substr(2,8),
-                page: name
-            });
-        }
+    const responseMessage = JSON.stringify({
+        name: "Carlos",
+        description: "This is something"
+    });
+
+    //const responseMessage = JSON.stringify(context.bindings.outputDocument, null, 4);
+
+        // if (name) {
+        //     context.bindings.outputDocument = JSON.stringify({
+        //         // create a random ID
+        //         id: new Date().toISOString() + Math.random().toString().substr(2,8),
+        //         page: name
+        //     });
+        // }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
